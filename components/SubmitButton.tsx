c46db6d0e0f08c40cbf8cda7641b41c4c4d4b2dc
Nick Black <nickblack@VELO-V201849.local>
2024-02-12 11:44:39 -0600

    initial submit button component

---
+++ components/SubmitButton.tsx
@@ -0,0 +1,25 @@
+'use client';
+import React from 'react';
+import { Button } from './ui/button';
+import { ReloadIcon } from '@radix-ui/react-icons';
+import { useFormStatus } from 'react-dom';
+
+type Props = {
+	children: React.ReactNode;
+	tabIndex?: number;
+};
+
+// export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement>
+
+const SubmitButton = ({ children, tabIndex }: Props) => {
+	const { pending } = useFormStatus();
+
+	return (
+		<Button tabIndex={tabIndex} disabled={pending}>
+			{pending && <ReloadIcon className='mr-2 h-4 w-4 animate-spin' />}
+			{children}
+		</Button>
+	);
+};
+
+export default SubmitButton;
