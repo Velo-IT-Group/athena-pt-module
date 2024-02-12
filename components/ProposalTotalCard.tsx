ff35991af25c70915edfcdfd2d091dd65f0a75d9
Nick Black <nickblack@VELO-V201849.local>
2024-02-12 12:06:40 -0600

    added server action to proposal update

--- components/ProposalTotalCard.tsx
+++ components/ProposalTotalCard.tsx
@@ -1,10 +1,17 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+'use client';
+import { handleProposalUpdate } from '@/app/actions';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
 type Props = {
 	proposal: Proposal;
 };
+
+let USDollar = new Intl.NumberFormat('en-US', {
+	style: 'currency',
+	currency: 'USD',
+});
 
 const ProposalTotalCard = ({ proposal }: Props) => {
 	return (
@@ -14,49 +21,35 @@ const ProposalTotalCard = ({ proposal }: Props) => {
 				<CardDescription>Deploy your new project in one-click.</CardDescription>
 			</CardHeader>
 			<CardContent>
-				<form>
+				<form action={handleProposalUpdate}>
 					<div className='grid w-full items-center gap-4'>
+						<input name='id' defaultValue={proposal.id} className='hidden' />
 						<div className='flex flex-col space-y-1.5'>
 							<Label htmlFor='total-labor-hours'>Total Labor Hours</Label>
-							<Input
-								id='total-labor-hours'
-								pattern='^\$\d{1,3}(,\d{3})*(\.\d+)?$'
-								type='number'
-								value={proposal.total_labor_hours}
-								placeholder='Total Labor Hours'
-								readOnly
-							/>
+							<p className='text-sm font-medium text-muted-foreground'>{proposal.labor_hours}</p>
 						</div>
 						<div className='flex flex-col space-y-1.5'>
 							<Label htmlFor='sales-labor'>Sales Ticket and Proposal Work</Label>
-							<Input id='sales-labor' type='number' value={proposal.sales_hours} placeholder='Total Labor Hours' readOnly />
+							<Input name='sales-labor' type='number' defaultValue={proposal.sales_hours} placeholder='Total Labor Hours' />
 						</div>
 						<div className='flex flex-col space-y-1.5'>
 							<Label htmlFor='project-management'>Project Management</Label>
-							<Input id='project-management' type='number' value={proposal.management_hours} placeholder='Total Labor Hours' readOnly />
+							<Input name='project-management' type='number' defaultValue={proposal.management_hours} placeholder='Total Labor Hours' />
 						</div>
 						<div className='flex flex-col space-y-1.5'>
 							<Label htmlFor='hours-required'>Hours Required</Label>
-							<Input id='hours-required' type='number' value={proposal.total_labor_hours} placeholder='Total Labor Hours' readOnly />
+							<p className='text-sm font-medium text-muted-foreground'>{proposal.hours_required}</p>
 						</div>
 						<div className='flex flex-col space-y-1.5'>
 							<Label htmlFor='labor-rate'>Labor Rate</Label>
-							<Input id='hours-required' type='number' value={proposal.labor_rate} placeholder='Total Labor Hours' readOnly />
+							<p className='text-sm font-medium text-muted-foreground'>{USDollar.format(Number(proposal.labor_rate))}</p>
 						</div>
 						<div className='flex flex-col space-y-1.5'>
 							<Label htmlFor='total-labor-price'>Total Labor Price</Label>
-							<Input
-								id='total-labor-price'
-								pattern='^\$\d{1,3}(,\d{3})*(\.\d+)?$'
-								type='text'
-								datatype='currency'
-								// onKeyUp={}
-								value={proposal.total_labor_price}
-								placeholder='Total Labor Hours'
-								readOnly
-							/>
+							<p className='text-sm font-medium text-muted-foreground'>{USDollar.format(Number(proposal.total_labor_price))}</p>
 						</div>
 					</div>
+					<input type='submit' className='hidden' />
 				</form>
 			</CardContent>
 		</Card>
