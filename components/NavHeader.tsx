1d9865839d790838c95b4edb1b50e72014818f35
Nick Black <nickblack@VELO-V201849.local>
2024-02-12 11:55:23 -0600

    changed src to relative logo

--- components/NavHeader.tsx
+++ components/NavHeader.tsx
@@ -7,7 +7,7 @@ const NavHeader = () => {
 		<header className='w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
 			<div className='container flex h-14 max-w-screen-2xl items-center'>
 				<Link href='/'>
-					<Image src='velo-logo-black.svg' alt='velo-logo' width={64} height={64} />
+					<Image src='/velo-logo-black.svg' alt='velo-logo' width={64} height={64} />
 				</Link>
 				<nav></nav>
 			</div>
