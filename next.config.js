7a8f28fffac347d7bca0594be350b285cf399e8a
Nick Black <nickblack@VELO-V201849.local>
2024-02-12 11:07:30 -0600

    added config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	reactStrictMode: false, // turn to false
+	devIndicators: {
+		autoPrerender: false,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
