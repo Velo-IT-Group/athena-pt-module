2ea956621ffeae60f54d0158cc95170250ee0e4c
Nick Black <nickblack@VELO-V201849.local>
2024-02-12 12:05:35 -0600

    added drag and drop for templates

--- components/TemplatePicker.tsx
+++ components/TemplatePicker.tsx
@@ -1,6 +1,11 @@
-import { ProjectTemplate } from '@/types';
+'use client';
 import React from 'react';
-import { Checkbox } from './ui/checkbox';
+import { ProjectTemplate } from '@/types/manage';
+import { DragHandleDots2Icon } from '@radix-ui/react-icons';
+import { ScrollArea } from './ui/scroll-area';
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from './ui/collapsible';
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
+import { Draggable, Droppable } from 'react-beautiful-dnd';
 
 type Props = {
 	templates: Array<ProjectTemplate>;
@@ -8,14 +13,43 @@ type Props = {
 
 const TemplatePicker = ({ templates }: Props) => {
 	return (
-		<>
-			{templates.map((template) => (
-				<div className='flex items-center gap-2' key={template.id}>
-					<Checkbox />
-					{template.name}
-				</div>
-			))}
-		</>
+		<Collapsible>
+			<Card>
+				<CollapsibleTrigger>
+					<CardHeader>
+						<CardTitle>Templates</CardTitle>
+					</CardHeader>
+				</CollapsibleTrigger>
+				<CollapsibleContent>
+					<ScrollArea className='h-80'>
+						<Droppable droppableId='templates' type='group'>
+							{(provided) => (
+								<CardContent {...provided.droppableProps} ref={provided.innerRef}>
+									{templates.map((template, index) => (
+										<Draggable key={template.id} draggableId={String(template.id)} index={index}>
+											{(provided) => {
+												return (
+													<div
+														key={template.id}
+														ref={provided.innerRef}
+														{...provided.draggableProps}
+														{...provided.dragHandleProps}
+														className='grid grid-cols-2 gap-2'
+													>
+														<DragHandleDots2Icon className='w-4 h-4' />
+														<p className='text-sm tracking-tight line-clamp-1'>{template.name}</p>
+													</div>
+												);
+											}}
+										</Draggable>
+									))}
+								</CardContent>
+							)}
+						</Droppable>
+					</ScrollArea>
+				</CollapsibleContent>
+			</Card>
+		</Collapsible>
 	);
 };
 
