3a8c7aadf26033586821e3e030ca6a055dda9b3e
Nick Black <nickblack@VELO-V201849.local>
2024-02-12 11:41:42 -0600

    login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,54 @@
 import React from 'react';
 import AuthForm from '@/components/AuthForm';
+import Link from 'next/link';
+import VeloLogo from '@/components/VeloLogo';
 
-const LoginPage = () => {
+const LoginPage = ({
+	searchParams,
+}: {
+	searchParams?: {
+		query?: string;
+		page?: string;
+	};
+}) => {
 	return (
-		<div className='row'>
-			<div className='col-6'>
-				<h1 className='header'>Supabase Auth + Storage</h1>
-				<p>
-					Experience our Auth and Storage through a simple profile management example. Create a user profile and upload an avatar image. Fast, simple,
-					secure.
-				</p>
+		<div className='container relative hidden h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0'>
+			<div className='relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r'>
+				<div className='absolute inset-0 bg-zinc-900' />
+				<div className='relative z-20 flex items-center text-lg font-medium'>
+					<VeloLogo classname='mr-2 h-6 w-6 ' />
+					Velo
+				</div>
+				<div className='relative z-20 mt-auto'>
+					<blockquote className='space-y-2'>
+						<p className='text-lg'>
+							&ldquo;We&apos;ve been with Velo IT Group for several years now, and these guys are the real thing. They are highly responsive,
+							committed to excellence, and they&apos;ve helped us to modernize our IT infrastructure across multiple states. Velo delivered on all of
+							their promises, and we couldn&apos;t be happier to work with them.&rdquo;
+						</p>
+						<footer className='text-sm'>Todd Trahan - President, Cal-Chlor</footer>
+					</blockquote>
+				</div>
 			</div>
-			<div className='col-6 auth-widget'>
-				<AuthForm />
+			<div className='lg:p-8'>
+				<div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
+					<div className='flex flex-col space-y-2 text-center'>
+						<h1 className='text-2xl font-semibold tracking-tight'>Create an account</h1>
+						<p className='text-sm text-muted-foreground'>Enter your email below to create your account</p>
+					</div>
+					<AuthForm />
+					<p className='px-8 text-center text-sm text-muted-foreground'>
+						By clicking continue, you agree to our{' '}
+						<Link href='/terms' className='underline underline-offset-4 hover:text-primary'>
+							Terms of Service
+						</Link>{' '}
+						and{' '}
+						<Link href='/privacy' className='underline underline-offset-4 hover:text-primary'>
+							Privacy Policy
+						</Link>
+						.
+					</p>
+				</div>
 			</div>
 		</div>
 	);
