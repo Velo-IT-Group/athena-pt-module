2569e7c488d65baf32dae0f4e8bf9baf77de8d21
Nick Black <nicholas.black98@icloud.com>
2024-02-17 11:11:23 -0600

    simplified file to include path array

--- components/ProposalTabs.tsx
+++ components/ProposalTabs.tsx
@@ -6,31 +6,20 @@ import React from 'react';
 
 const ProposalTabs = ({ id }: { id: string }) => {
 	const pathname = usePathname();
+
+	const paths = [`/proposal/${id}`, `/proposal/${id}/workplan`, `/proposal/${id}/products`, `/proposal/${id}/settings`];
+
 	return (
 		<div className='flex items-center gap-4 w-full px-4 border-b bg-gray-100/40 dark:bg-gray-800/40'>
-			<Link
-				href={`/proposal/${id}`}
-				className={cn('py-2', pathname === `/proposal/${id}` ? 'border-b-2 border-primary' : 'border-b-2 border-none text-muted-foreground')}
-			>
-				Overview
-			</Link>
-			<Link
-				href={`/proposal/${id}/workplan`}
-				className={cn('py-2', pathname === `/proposal/${id}/workplan` ? 'border-b-2 border-primary' : 'border-b-2 border-none text-muted-foreground')}
-			>
-				Workplan
-			</Link>
-			<Link
-				href={`/proposal/${id}/products`}
-				className={cn(
-					'py-2',
-					pathname === `/proposal/${id}/products`
-						? 'border-b-2 border-primary text-accent-foreground'
-						: 'border-b-2 border-none text-muted-foreground'
-				)}
-			>
-				Products
-			</Link>
+			{paths.map((path, index) => (
+				<Link
+					href={path}
+					className={cn('py-2', pathname === path ? 'border-b-2 border-primary' : 'border-b-2 border-none text-muted-foreground')}
+					key={index}
+				>
+					Overview
+				</Link>
+			))}
 		</div>
 	);
 };
