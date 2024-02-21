76a5ef2c94a92552627e207ab886f00d737f02d5
Nick Black <nicholas.black98@icloud.com>
2024-02-20 22:10:04 -0600

    removed unnecessary dependencies

--- app/(org)/proposal/[id]/workplan/SectionsList.tsx
+++ app/(org)/proposal/[id]/workplan/SectionsList.tsx
@@ -1,21 +1,10 @@
 'use client';
-import React, { useOptimistic, useTransition } from 'react';
+import React from 'react';
 import SectionListItem from './SectionListItem';
 import { Draggable, Droppable, DroppableStateSnapshot } from 'react-beautiful-dnd';
 import { cn } from '@/lib/utils';
-import { handleSectionInsert } from '@/app/actions';
-import { Button } from '@/components/ui/button';
-import { PlusIcon } from '@radix-ui/react-icons';
 
 const SectionsList = ({ id, sections }: { id: string; sections: NestedSection[] }) => {
-	const sectionStub: NestedSection = { created_at: Date(), id: '123459', name: 'New Section', order: 1, proposal: id, phases: [] };
-
-	const [isPending, startTransition] = useTransition();
-	const [optimisticSections, addOptimisticSection] = useOptimistic<NestedSection[], NestedSection>(
-		sections,
-		(state: NestedSection[], newPhase: NestedSection) => [...state, newPhase]
-	);
-
 	const getBackgroundColor = (snapshot: DroppableStateSnapshot): string => {
 		// Giving isDraggingOver preference
 		if (snapshot.isDraggingOver) {
@@ -35,12 +24,12 @@ const SectionsList = ({ id, sections }: { id: string; sections: NestedSection[] 
 		<Droppable droppableId='sections'>
 			{(provided, snapshot) => (
 				<div {...provided.droppableProps} ref={provided.innerRef} className={cn('space-y-4 rounded-xl h-full', getBackgroundColor(snapshot))}>
-					{optimisticSections.map((section, index) => (
+					{sections.map((section, index) => (
 						<Draggable key={section.id} draggableId={section.id} index={index}>
 							{(provided) => {
 								return (
 									<div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-										<SectionListItem key={section.id} section={section} phases={section.phases} />
+										<SectionListItem key={section.id} section={section} phases={section.phases ?? []} />
 									</div>
 								);
 							}}
