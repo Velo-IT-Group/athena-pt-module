8c1168207f563fa2e9b8b4225acb105d4df5dbdd
Nick Black <nicholas.black98@icloud.com>
2024-02-20 23:07:35 -0600

    added catalog items to query

--- app/(org)/proposal/[id]/products/page.tsx
+++ app/(org)/proposal/[id]/products/page.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { getProducts } from '@/lib/data';
+import { getCatalogItems, getProducts } from '@/lib/data';
 import { DataTable } from './data-table';
 import { columns } from './columns';
 
-const ProposalProductPage = async () => {
-	const products = await getProducts();
+const ProposalProductPage = async ({ params }: { params: { id: string } }) => {
+	const products = await getProducts(params.id);
+	console.log(products, params.id);
+	const catalogItems = await getCatalogItems();
 
 	return (
 		<div className='bg-muted/50 h-full'>
 			<div className='container space-y-4 '>
 				<h1 className='text-xl font-semibold'>Products</h1>
-				<DataTable columns={columns} data={products ?? []} />
+				<DataTable columns={columns} data={catalogItems ?? []} products={products ?? []} id={params.id} />
 			</div>
 		</div>
 	);
