c5922c9de1e4f8809eec021fa415c1ce471e4961
Nick Black <nickblack@VELO-V201849.local>
2024-02-23 17:15:28 -0600

    added a parsing function to extract number from string

--- utils/money.ts
+++ utils/money.ts
@@ -6,3 +6,13 @@ export const getCurrencyString = (value: number) => {
 
 	return USDollar.format(value);
 };
+
+export const parseAmount = (value: string) => {
+	const re = /\$([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?/;
+	const ok = re.exec(value);
+	console.log(ok);
+	if (!re.exec(value)) return;
+	const split = value.split('$');
+	if (split.length <= 1) return;
+	return parseFloat(split[1]);
+};
