3edb074a68fdf069425e6d27d0f9c10c2dd808f6
Nick Black <nickblack@VELO-V201849.local>
2024-02-23 10:06:52 -0600

    added base config

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { AxiosRequestConfig } from 'axios';
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+export const baseConfig: AxiosRequestConfig = {
+	method: 'get',
+	baseURL: process.env.NEXT_PUBLIC_CW_URL!,
+	headers: {
+		clientId: process.env.NEXT_PUBLIC_CW_CLIENT_ID!,
+	},
+	auth: {
+		username: process.env.NEXT_PUBLIC_CW_USERNAME!,
+		password: process.env.NEXT_PUBLIC_CW_PASSWORD!,
+	},
+};
