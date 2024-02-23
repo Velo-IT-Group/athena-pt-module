278e1a28e233bf5f4cda19e7a8f896d8a9b98391
Nick Black <nickblack@VELO-V201849.local>
2024-02-23 08:51:44 -0600

    added database

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -2,7 +2,7 @@ import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
-	return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+	return createServerClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
 		cookies: {
 			get(name: string) {
 				return cookieStore.get(name)?.value;
