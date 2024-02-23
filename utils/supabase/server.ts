fb83433f2fd61b5cfa11d16ce18d19c660dc4242
Nick Black <nickblack@VELO-V201849.local>
2024-02-23 08:44:26 -0600

    now passing cookie store into client

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,10 +1,8 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
-export const createClient = () => {
-	const cookieStore = cookies();
-
-	return createServerClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+	return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
 		cookies: {
 			get(name: string) {
 				return cookieStore.get(name)?.value;
