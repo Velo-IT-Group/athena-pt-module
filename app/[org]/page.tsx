f3969280d743ed562367c6720088ac4796c8931b
Nick Black <nicholas.black98@icloud.com>
2024-02-22 22:24:47 -0600

    replaced page with org overview page

--- app/[org]/page.tsx
+++ app/[org]/page.tsx
@@ -1,221 +1,138 @@
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { DotsHorizontalIcon, PlusIcon } from '@radix-ui/react-icons';
 import React from 'react';
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import ResponsiveBarChart from '@/components/ResponsiveBarChart';
-import DatePicker from '@/components/DatePicker';
-import DatePickerWithRange from '@/components/DatePickerWithRange';
+import type { Metadata, ResolvingMetadata } from 'next';
+import { getOrganization, getProposals } from '@/lib/data';
+import { Button } from '@/components/ui/button';
+import { CalendarIcon, DotsHorizontalIcon, DropdownMenuIcon, MagnifyingGlassIcon, PlusIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { relativeDate } from '@/utils/date';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { Input } from '@/components/ui/input';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Label } from '@/components/ui/label';
+import { getCurrencyString } from '@/utils/money';
+import Navbar, { Tab } from '@/components/Navbar';
+import { cn } from '@/lib/utils';
 
-const data = [
-	{
-		goal: 400,
-	},
-	{
-		goal: 300,
-	},
-	{
-		goal: 200,
-	},
-	{
-		goal: 300,
-	},
-	{
-		goal: 200,
-	},
-	{
-		goal: 278,
-	},
-	{
-		goal: 189,
-	},
-	{
-		goal: 239,
-	},
-	{
-		goal: 300,
-	},
-	{
-		goal: 200,
-	},
-	{
-		goal: 278,
-	},
-	{
-		goal: 189,
-	},
-	{
-		goal: 349,
-	},
-];
+type Props = {
+	params: { org: string };
+	searchParams: { [key: string]: string | string[] | undefined };
+};
 
-const OverviewPage = () => {
+export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+	// fetch data
+	const organization = await getOrganization(params.org);
+
+	// optionally access and extend (rather than replace) parent metadata
+	const previousImages = (await parent).openGraph?.images || [];
+
+	return {
+		title: `Quotes - ${organization?.name ?? 'Unknown'}`,
+	};
+}
+
+const OverviewPage = async ({ params }: Props) => {
+	const proposals = await getProposals();
+
+	const orgDashboardTabs: Tab[] = [
+		{
+			name: 'Overview',
+			href: `/${params.org}`,
+		},
+		{
+			name: 'Integrations',
+			href: `/${params.org}/integrations`,
+		},
+		{
+			name: 'Activity',
+			href: `/${params.org}/integrations`,
+		},
+		{
+			name: 'Settings',
+			href: `/${params.org}/settings`,
+		},
+	];
+
+	if (!proposals) return <div></div>;
+
 	return (
-		<div className='container py-12 space-y-8'>
-			<header className='flex items-center justify-between gap-4'>
-				<div>
-					<h1 className='text-3xl font-semibold tracking-tight'>Overview</h1>
-					<h2 className='text-muted-foreground'>Last 14 days</h2>
+		<>
+			<Navbar org={params.org} tabs={orgDashboardTabs} />
+			<div className='min-h-header bg-muted/75'>
+				<div className='container py-8 space-y-8'>
+					<form method='GET' className='flex gap-4 items-center w-full'>
+						<div
+							className='flex h-9 items-center w-full rounded-md border border-input bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50'
+							cmdk-input-wrapper=''
+						>
+							<MagnifyingGlassIcon className='mr-2 h-4 w-4 shrink-0 opacity-50' />
+							<Input placeholder='Search quotes' className='border-0 shadow-none focus-visible:ring-0' />
+						</div>
+
+						<Select>
+							<SelectTrigger className='w-48 bg-background'>
+								<SelectValue placeholder='Sort by' />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectItem value='activity'>Sort by activity</SelectItem>
+								<SelectItem value='name'>Sort by name</SelectItem>
+							</SelectContent>
+						</Select>
+						<Button asChild>
+							<Link href='/proposal/new'>
+								<PlusIcon className='w-4 h-4 mr-2' /> Add New
+							</Link>
+						</Button>
+					</form>
+
+					<div className='grid grid-cols-3 gap-4'>
+						{proposals.map((proposal) => (
+							<Card key={proposal.id} className='flex flex-col justify-stretch h-full'>
+								<CardHeader className='flex-row items-start justify-between -space-y-2'>
+									<div>
+										<Link href={`${params.org}/proposal/${proposal.id}`}>
+											<CardTitle className='text-sm hover:underline'>{proposal.name}</CardTitle>
+										</Link>
+										<Link href={`/review/${proposal.id}`}>
+											<CardDescription className='line-clamp-1 hover:underline'>localhost:3000/review/{proposal.id}</CardDescription>
+										</Link>
+									</div>
+									<DropdownMenu>
+										<DropdownMenuTrigger asChild>
+											<Button variant='ghost' size='sm'>
+												<DotsHorizontalIcon className='w-4 h-4' />
+											</Button>
+										</DropdownMenuTrigger>
+										<DropdownMenuContent>
+											<DropdownMenuItem>Add To Favorites</DropdownMenuItem>
+											<DropdownMenuItem>Settings</DropdownMenuItem>
+											<DropdownMenuSeparator />
+											<DropdownMenuItem>Duplicate</DropdownMenuItem>
+											<DropdownMenuSeparator />
+											<DropdownMenuItem className='text-red-600'>Delete</DropdownMenuItem>
+										</DropdownMenuContent>
+									</DropdownMenu>
+								</CardHeader>
+								<CardContent className='grid grid-cols-2 gap-2'>
+									<div className='grid w-full max-w-sm items-center gap-1.5'>
+										<Label>Company</Label>
+										<p className='text-sm text-muted-foreground'>{proposal?.company_name ?? 'Company Name'}</p>
+									</div>
+									<div className='grid w-full max-w-sm items-center gap-1.5'>
+										<Label>Total</Label>
+										<p className='text-sm text-muted-foreground'>{getCurrencyString(proposal.total_labor_price)}</p>
+									</div>
+								</CardContent>
+								<CardFooter className='mt-auto'>
+									<CalendarIcon className='h-3 w-3 mr-2' />
+									<p className='text-sm text-muted-foreground'>{relativeDate(new Date(proposal.updated_at))}</p>
+								</CardFooter>
+							</Card>
+						))}
+					</div>
 				</div>
-				<div className='flex items-center gap-4'>
-					<DatePickerWithRange />
-					<Button>
-						<PlusIcon className='h-4 w-4 mr-2' /> Add New
-					</Button>
-					<DropdownMenu>
-						<DropdownMenuTrigger asChild>
-							<Button variant='outline' size='icon'>
-								<DotsHorizontalIcon className='h-4 w-4' />
-							</Button>
-						</DropdownMenuTrigger>
-						<DropdownMenuContent>
-							<DropdownMenuLabel>My Account</DropdownMenuLabel>
-							<DropdownMenuSeparator />
-							<DropdownMenuItem>Profile</DropdownMenuItem>
-							<DropdownMenuItem>Billing</DropdownMenuItem>
-							<DropdownMenuItem>Team</DropdownMenuItem>
-							<DropdownMenuItem>Subscription</DropdownMenuItem>
-						</DropdownMenuContent>
-					</DropdownMenu>
-				</div>
-			</header>
-
-			<div className='grid grid-cols-5 gap-2'>
-				<Card>
-					<CardHeader>
-						<CardTitle>Kubernetes costs</CardTitle>
-					</CardHeader>
-					<CardContent>
-						<div className='min-h-24'>
-							<p className='text-2xl font-semibold'>$101.99</p>
-						</div>
-					</CardContent>
-					<CardFooter>
-						<p className='text-sm text-muted-foreground'>Including 1 cluster</p>
-					</CardFooter>
-				</Card>
-				<Card>
-					<CardHeader>
-						<CardTitle>Kubernetes costs</CardTitle>
-					</CardHeader>
-					<CardContent>
-						<div className='min-h-24'>
-							<p className='text-2xl font-semibold'>$101.99</p>
-						</div>
-					</CardContent>
-					<CardFooter>
-						<p className='text-sm text-muted-foreground'>Including 1 cluster</p>
-					</CardFooter>
-				</Card>
-				<Card>
-					<CardHeader>
-						<CardTitle>Kubernetes costs</CardTitle>
-					</CardHeader>
-					<CardContent>
-						<div className='min-h-24'>
-							<p className='text-2xl font-semibold'>$101.99</p>
-						</div>
-					</CardContent>
-					<CardFooter>
-						<p className='text-sm text-muted-foreground'>Including 1 cluster</p>
-					</CardFooter>
-				</Card>
-				<Card>
-					<CardHeader>
-						<CardTitle>Kubernetes costs</CardTitle>
-					</CardHeader>
-					<CardContent>
-						<div className='min-h-24'>
-							<p className='text-2xl font-semibold'>$101.99</p>
-						</div>
-					</CardContent>
-					<CardFooter>
-						<p className='text-sm text-muted-foreground'>Including 1 cluster</p>
-					</CardFooter>
-				</Card>
-				<Card>
-					<CardHeader>
-						<CardTitle>Kubernetes costs</CardTitle>
-					</CardHeader>
-					<CardContent>
-						<div className='min-h-24'>
-							<p className='text-2xl font-semibold'>$101.99</p>
-						</div>
-					</CardContent>
-					<CardFooter>
-						<p className='text-sm text-muted-foreground'>Including 1 cluster</p>
-					</CardFooter>
-				</Card>
 			</div>
-
-			<h3 className='text-2xl font-semibold'>Clusters</h3>
-
-			<div className='grid grid-cols-2 gap-4 h-full'>
-				<Card>
-					<CardHeader className='flex-row items-center justify-between gap-4 w-full border-b'>
-						<CardTitle>Kubernetes costs</CardTitle>
-						<DropdownMenu>
-							<DropdownMenuTrigger asChild>
-								<Button variant='outline' size='icon'>
-									<DotsHorizontalIcon className='h-4 w-4' />
-								</Button>
-							</DropdownMenuTrigger>
-							<DropdownMenuContent>
-								<DropdownMenuLabel>My Account</DropdownMenuLabel>
-								<DropdownMenuSeparator />
-								<DropdownMenuItem>Profile</DropdownMenuItem>
-								<DropdownMenuItem>Billing</DropdownMenuItem>
-								<DropdownMenuItem>Team</DropdownMenuItem>
-								<DropdownMenuItem>Subscription</DropdownMenuItem>
-							</DropdownMenuContent>
-						</DropdownMenu>
-					</CardHeader>
-					<CardContent>
-						<div className='min-h-24'>
-							<p className='text-2xl font-semibold'>$101.99</p>
-						</div>
-					</CardContent>
-					<CardFooter>
-						<p className='text-sm text-muted-foreground'>Including 1 cluster</p>
-					</CardFooter>
-				</Card>
-				<Card>
-					<CardHeader className='flex-row items-center justify-between gap-4 w-full border-b'>
-						<CardTitle>Kubernetes costs</CardTitle>
-						<DropdownMenu>
-							<DropdownMenuTrigger asChild>
-								<Button variant='outline' size='icon'>
-									<DotsHorizontalIcon className='h-4 w-4' />
-								</Button>
-							</DropdownMenuTrigger>
-							<DropdownMenuContent>
-								<DropdownMenuLabel>My Account</DropdownMenuLabel>
-								<DropdownMenuSeparator />
-								<DropdownMenuItem>Profile</DropdownMenuItem>
-								<DropdownMenuItem>Billing</DropdownMenuItem>
-								<DropdownMenuItem>Team</DropdownMenuItem>
-								<DropdownMenuItem>Subscription</DropdownMenuItem>
-							</DropdownMenuContent>
-						</DropdownMenu>
-					</CardHeader>
-					<CardContent>
-						<div className='mt-3 h-[120px]'>
-							<ResponsiveBarChart data={data} />
-						</div>
-					</CardContent>
-					<CardFooter>
-						<p className='text-sm text-muted-foreground'>Including 1 cluster</p>
-					</CardFooter>
-				</Card>
-			</div>
-		</div>
+		</>
 	);
 };
 
