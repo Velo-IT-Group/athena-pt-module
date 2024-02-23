9c99bb35b983c84cc2dcc9c311df57b5e8fbd957
Nick Black <nickblack@VELO-V201849.local>
2024-02-23 10:36:51 -0600

    added updateProduct function

--- lib/functions/update.ts
+++ lib/functions/update.ts
@@ -2,6 +2,19 @@
 import { createClient } from '@/utils/supabase/server';
 import { revalidateTag } from 'next/cache';
 
+export const updateProduct = async (id: string, product: ProductUpdate) => {
+	const supabase = createClient();
+	const { error } = await supabase.from('products').update(product).eq('id', id);
+
+	if (error) {
+		console.error(error);
+		return;
+	}
+
+	revalidateTag('proposals');
+	revalidateTag('products');
+};
+
 export const updateProposal = async (id: string, proposal: ProposalUpdate) => {
 	const supabase = createClient();
 	const { error } = await supabase.from('proposals').update(proposal).eq('id', id);
