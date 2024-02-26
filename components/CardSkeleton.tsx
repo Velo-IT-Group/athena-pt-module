81433ba0e34b982b96dc1bdc793a68e49d7eef57
Nick Black <nicholas.black98@icloud.com>
2024-02-26 07:51:06 -0600

    removed fixed width but rather span entire parent

--- components/CardSkeleton.tsx
+++ components/CardSkeleton.tsx
@@ -3,8 +3,8 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 const CardSkeleton = () => {
 	return (
-		<div className='flex flex-col space-y-3'>
-			<Skeleton className='h-[125px] w-[250px] rounded-xl' />
+		<div className='flex flex-col h-full space-y-3'>
+			<Skeleton className='h-40 rounded-xl' />
 			<div className='space-y-2'>
 				<Skeleton className='h-4 w-[250px]' />
 				<Skeleton className='h-4 w-[200px]' />
