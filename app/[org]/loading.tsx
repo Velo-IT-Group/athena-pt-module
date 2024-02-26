0e2a64a5ad04b0378c5007e5b87765e1beef5bc4
Nick Black <nicholas.black98@icloud.com>
2024-02-26 07:51:30 -0600

    initial commit on loading pages

---
+++ app/[org]/loading.tsx
@@ -0,0 +1,24 @@
+import CardSkeleton from '@/components/CardSkeleton';
+import Navbar from '@/components/Navbar';
+import React from 'react';
+
+const OrganizationLoadingPage = () => {
+	let blankArray = new Array(9).fill(null);
+
+	return (
+		<>
+			<Navbar org='' tabs={[]} />
+			<div className='min-h-header bg-muted/75'>
+				<div className='container py-8 space-y-8'>
+					<div className='grid grid-cols-3 gap-4'>
+						{blankArray.map((_, index) => (
+							<CardSkeleton key={index} />
+						))}
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default OrganizationLoadingPage;
