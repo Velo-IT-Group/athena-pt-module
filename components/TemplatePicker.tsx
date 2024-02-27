d6000899ca6f2d8ef916b5d266b479702368cac5
Nick Black <nickblack@VELO-V201849.local>
2024-02-27 17:20:13 -0600

    using generic dragging background function

--- components/TemplatePicker.tsx
+++ components/TemplatePicker.tsx
@@ -2,39 +2,24 @@
 import React from 'react';
 import { ProjectTemplate } from '@/types/manage';
 import { DragHandleDots2Icon } from '@radix-ui/react-icons';
-import { Draggable, Droppable, DroppableStateSnapshot } from 'react-beautiful-dnd';
-import { Card, CardContent, CardHeader } from './ui/card';
-import { cn } from '@/lib/utils';
+import { Draggable, Droppable } from 'react-beautiful-dnd';
+import { Card, CardHeader } from './ui/card';
+import { cn, getBackgroundColor } from '@/lib/utils';
 
 type Props = {
 	templates: Array<ProjectTemplate>;
 };
 
 const TemplatePicker = ({ templates }: Props) => {
-	const getBackgroundColor = (snapshot: DroppableStateSnapshot): string => {
-		// Giving isDraggingOver preference
-		if (snapshot.isDraggingOver) {
-			return 'bg-pink-50';
-		}
-
-		// If it is the home list but not dragging over
-		if (snapshot.draggingFromThisWith) {
-			return 'bg-blue-50';
-		}
-
-		// Otherwise use our default background
-		return 'bg-transparent';
-	};
-
 	return (
-		<Droppable droppableId='templates'>
+		<Droppable droppableId='templates' type='group'>
 			{(provided, snapshot) => (
 				<div {...provided.droppableProps} ref={provided.innerRef} className={cn('space-y-2 rounded-xl', getBackgroundColor(snapshot))}>
 					{templates.map((template, index) => (
 						<Draggable key={template.id} draggableId={String(template.id)} index={index}>
 							{(provided) => {
 								return (
-									<div key={template.id} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className=''>
+									<div key={template.id} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
 										<Card>
 											<CardHeader className='flex flex-row items-center gap-2 p-2 space-y-0'>
 												<DragHandleDots2Icon className='w-4 h-4 flex-shrink-0' />
