5594bd714944c1a9c9ea6e07b774d99b30f53f95
Nick Black <nickblack@VELO-V201849.local>
2024-02-27 17:22:08 -0600

    made functions generic and enforceable with order variable

--- utils/array.ts
+++ utils/array.ts
@@ -1,23 +1,14 @@
-import { DropResult } from 'react-beautiful-dnd';
+import { DraggableLocation } from 'react-beautiful-dnd';
 
-export const reorder = (list: NestedPhase[] | NestedTicket[] | Task[], startIndex: number, endIndex: number) => {
+interface Orderable {
+	order: number;
+}
+
+export const reorder = <T extends Orderable>(list: T[], startIndex: number, endIndex: number): T[] => {
 	const result = [...list];
 	const [removed] = result.splice(startIndex, 1);
 	result.splice(endIndex, 0, removed);
 	result.forEach((item, index) => (item.order = index + 1));
-
-	const changedPhases: NestedPhase[] = [];
-	console.log(changedPhases);
-
-	for (var i = endIndex; i < result.length; i++) {
-		console.log(result[i], i);
-		if (!result[i].order) return;
-
-		result[i].order = i + 1;
-		changedPhases.push(result[i]);
-	}
-
-	console.log(changedPhases);
 
 	return result;
 };
@@ -45,7 +36,12 @@ export const reorder = (list: NestedPhase[] | NestedTicket[] | Task[], startInde
 /**
  * Moves an item from one list to another list.
  */
-export const move = (source: any, destination: any, droppableSource: any, droppableDestination: any) => {
+export const move = <T extends Orderable>(
+	source: T[],
+	destination: T[],
+	droppableSource: DraggableLocation,
+	droppableDestination: DraggableLocation
+) => {
 	const sourceClone = Array.from(source);
 	const destClone = Array.from(destination);
 	const [removed] = sourceClone.splice(droppableSource.index, 1);
@@ -53,7 +49,9 @@ export const move = (source: any, destination: any, droppableSource: any, droppa
 	destClone.splice(droppableDestination.index, 0, removed);
 
 	const result = {};
+	// @ts-ignore
 	result[droppableSource.droppableId] = sourceClone;
+	// @ts-ignore
 	result[droppableDestination.droppableId] = destClone;
 
 	return result;
