f598cd2c5997ec3398ec0bc9e1b96d6e8cd4cda7
Nick Black <nickblack@VELO-V201849.local>
2024-02-27 17:20:45 -0600

    removed signin / signup function to extenal file

--- components/forms/AuthForm.tsx
+++ components/forms/AuthForm.tsx
@@ -1,55 +1,11 @@
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
-import { headers } from 'next/headers';
-import { createClient } from '@/utils/supabase/server';
-import { redirect } from 'next/navigation';
 import SubmitButton from '@/components/SubmitButton';
+import { signIn } from '@/lib/functions/read';
+import { signUp } from '@/lib/functions/create';
 
 export default function AuthForm({ searchParams }: { searchParams?: { message: string } }) {
-	const signIn = async (formData: FormData) => {
-		'use server';
-
-		const email = formData.get('email') as string;
-		const password = formData.get('password') as string;
-		const supabase = createClient();
-
-		const { error } = await supabase.auth.signInWithPassword({
-			email,
-			password,
-		});
-
-		if (error) {
-			console.error(error);
-			return redirect('/login?message=Could not authenticate user');
-		}
-
-		return redirect('/velo-it-group');
-	};
-
-	const signUp = async (formData: FormData) => {
-		'use server';
-
-		const origin = headers().get('origin');
-		const email = formData.get('email') as string;
-		const password = formData.get('password') as string;
-		const supabase = createClient();
-
-		const { error } = await supabase.auth.signUp({
-			email,
-			password,
-			options: {
-				emailRedirectTo: `${origin}/auth/callback`,
-			},
-		});
-
-		if (error) {
-			return redirect('/login?message=Could not authenticate user');
-		}
-
-		return redirect('/login?message=Check email to continue sign in process');
-	};
-
 	return (
 		<form className='animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground' action={signIn}>
 			<Label htmlFor='email'>Email</Label>
