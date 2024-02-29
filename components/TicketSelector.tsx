3954e845852b54e1c328a0fdcabd94215e4463cf
Nick Black <nickblack@VELO-V201849.local>
2024-02-29 08:54:05 -0600

    now selects default value if there is one

--- components/TicketSelector.tsx
+++ components/TicketSelector.tsx
@@ -5,14 +5,14 @@ import * as React from 'react';
 import { type ServiceTicket } from '@/types/manage';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
 
-const TicketSelector = ({ tickets }: { tickets?: ServiceTicket[] }) => {
+const TicketSelector = ({ tickets, ticket }: { tickets: ServiceTicket[]; ticket?: number | null }) => {
 	return (
-		<Select name='service_ticket'>
+		<Select name='service_ticket' defaultValue={String(ticket)}>
 			<SelectTrigger className='col-span-3' tabIndex={2}>
 				<SelectValue placeholder='Select a ticket' />
 			</SelectTrigger>
 			<SelectContent>
-				{tickets?.map((ticket) => (
+				{tickets.map((ticket) => (
 					// @ts-ignore
 					<SelectItem key={ticket.id} value={String(ticket.id)}>
 						#{ticket.id} - {ticket.summary}
