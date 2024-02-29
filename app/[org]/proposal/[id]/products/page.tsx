452fb93b3d19cac5f4185b6b23eea436364fab01
Nick Black <nickblack@VELO-V201849.local>
2024-02-29 08:55:46 -0600

    added dialog to add new products

--- app/[org]/proposal/[id]/products/page.tsx
+++ app/[org]/proposal/[id]/products/page.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { getCatalogItems, getProducts } from '@/lib/functions/read';
 import { DataTable } from './data-table';
 import { columns } from './columns';
+import { Button } from '@/components/ui/button';
+import { PlusCircledIcon } from '@radix-ui/react-icons';
+import { Dialog, DialogTrigger } from '@/components/ui/dialog';
+import CatalogPicker from './catalog-picker';
 
 const ProposalProductPage = async ({ params }: { params: { id: string } }) => {
 	const products = await getProducts(params.id);
@@ -12,12 +16,24 @@ const ProposalProductPage = async ({ params }: { params: { id: string } }) => {
 	}
 
 	return (
-		<div className='bg-muted/50 h-full'>
-			<div className='container space-y-4 '>
-				<h1 className='text-xl font-semibold'>Products</h1>
-				<DataTable columns={columns} data={catalogItems ?? []} products={products ?? []} id={params.id} />
+		<>
+			<div className='bg-background py-10 border-b'>
+				<div className='flex items-center justify-between container'>
+					<h1 className='text-3xl font-medium'>Products</h1>
+					<Dialog>
+						<DialogTrigger asChild>
+							<Button>
+								<PlusCircledIcon className='h-4 w-4 mr-2' /> Add Product
+							</Button>
+						</DialogTrigger>
+						<CatalogPicker catalogItems={catalogItems} />
+					</Dialog>
+				</div>
 			</div>
-		</div>
+			<div className='container py-10'>
+				<DataTable columns={columns} data={products ?? []} />
+			</div>
+		</>
 	);
 };
 
