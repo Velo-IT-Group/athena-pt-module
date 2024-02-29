a7a82ab063bd6745a7252213924d70f9d547ddf0
Nick Black <nickblack@VELO-V201849.local>
2024-02-29 08:54:58 -0600

    made form action inline

--- app/[org]/proposal/new/page.tsx
+++ app/[org]/proposal/new/page.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { CardTitle, CardHeader, CardContent, Card, CardFooter } from '@/components/ui/card';
 import NewProposalForm from '@/components/forms/NewProposalForm';
-import { getTemplates, getTickets } from '@/lib/functions/read';
+import { getOrganization, getTemplates, getTickets } from '@/lib/functions/read';
 import SubmitButton from '@/components/SubmitButton';
-import { handleProposalInsert } from '@/app/actions';
+import { createProposal } from '@/lib/functions/create';
 
 const NewProposalPage = async () => {
 	const templates = await getTemplates();
+	const organization = await getOrganization();
 	// console.log(templates);
 	const tickets = await getTickets();
 	// console.log('TICKETS RESPONSE', tickets);
+	if (!organization) return <div></div>;
 
 	return (
 		<Card>
-			<form action={handleProposalInsert}>
+			<form
+				action={async (data: FormData) => {
+					'use server';
+					const name = data.get('name') as string;
+					const templates_used = data.getAll('templates_used') as unknown as number[];
+					const service_ticket = data.get('service_ticket') as unknown as number;
+					// @ts-ignore
+					await createProposal({ name, templates_used: [...templates_used, organization?.default_template], service_ticket });
+				}}
+			>
 				<CardHeader>
 					<CardTitle>New Proposal</CardTitle>
 				</CardHeader>
