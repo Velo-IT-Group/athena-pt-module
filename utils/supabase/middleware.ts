7ef7be07f9d7fd39a7c5fb49dd31852dfc45c7e9
Nick Black <nickblack@VELO-V201849.local>
2024-06-27 09:18:02 -0500

    removed middleware complexity

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -8,82 +8,53 @@ export async function updateSession(request: NextRequest) {
 		},
 	});
 
-	try {
-		const supabase = createServerClient(
-			process.env.NEXT_PUBLIC_SUPABASE_URL!,
-			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-			{
-				cookies: {
-					get(name: string) {
-						return request.cookies.get(name)?.value;
-					},
-					set(name: string, value: string, options: CookieOptions) {
-						request.cookies.set({
-							name,
-							value,
-							...options,
-						});
-						response = NextResponse.next({
-							request: {
-								headers: request.headers,
-							},
-						});
-						response.cookies.set({
-							name,
-							value,
-							...options,
-						});
-					},
-					remove(name: string, options: CookieOptions) {
-						request.cookies.set({
-							name,
-							value: '',
-							...options,
-						});
-						response = NextResponse.next({
-							request: {
-								headers: request.headers,
-							},
-						});
-						response.cookies.set({
-							name,
-							value: '',
-							...options,
-						});
-					},
+	const supabase = createServerClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		{
+			cookies: {
+				get(name: string) {
+					return request.cookies.get(name)?.value;
 				},
-			}
-		);
-
-		const { data, error } = await supabase.auth.getUser();
-
-		if (error || !data.user) throw new Error(`No user signed in: ${error}`);
-
-		// if user is signed in and the current path is / redirect the user to /account
-		if (request.nextUrl.pathname === '/') {
-			console.log('is on homepage');
-			return NextResponse.redirect(new URL(`/velo-it-group`, request.url));
+				set(name: string, value: string, options: CookieOptions) {
+					request.cookies.set({
+						name,
+						value,
+						...options,
+					});
+					response = NextResponse.next({
+						request: {
+							headers: request.headers,
+						},
+					});
+					response.cookies.set({
+						name,
+						value,
+						...options,
+					});
+				},
+				remove(name: string, options: CookieOptions) {
+					request.cookies.set({
+						name,
+						value: '',
+						...options,
+					});
+					response = NextResponse.next({
+						request: {
+							headers: request.headers,
+						},
+					});
+					response.cookies.set({
+						name,
+						value: '',
+						...options,
+					});
+				},
+			},
 		}
+	);
 
-		if (request.nextUrl.pathname === '/login') {
-			return NextResponse.redirect(new URL(`/velo-it-group`, request.url));
-		}
-	} catch (e) {
-		// if user is not signed in and the current path is not / redirect the user to /
-		if (request.nextUrl.pathname.includes('review')) {
-			return response;
-		}
-
-		if (request.nextUrl.pathname === '/auth/callback') {
-			return response;
-		}
-
-		if (request.nextUrl.pathname === '/login') {
-			return response;
-		}
-
-		return NextResponse.redirect(new URL('/login', request.url));
-	}
+	await supabase.auth.getUser();
 
 	return response;
 }
